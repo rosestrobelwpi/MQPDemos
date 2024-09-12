dd984ae328f522769020ea25407b49ed57963d3f
unknown <rosestrobe@gmail.com>
2024-09-12 15:45:24 -0400

    added everything

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Quilt Designer</h1>
+      <div className="btn-container">
+        <div className="top-btns">
+          <button>Play</button>
+          <button>Tutorial</button>
+        </div>
+        <div className="bottom-btn">
+          <button>About Us</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
